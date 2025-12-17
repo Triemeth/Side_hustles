2fea4426de2570946bc49a22c656c438a24ef261
Ethan Triem <EJTriem@outlook.com>
2025-12-16 21:05:54 -0600

    i should work on this more

--- CFB_predictions_take_2/pre_model_pyfiles/tester.py
+++ CFB_predictions_take_2/pre_model_pyfiles/tester.py
@@ -1,22 +1,29 @@
 import pandas as pd
 
-def dup_cols(df):
-    opp_cols = [c for c in df.columns if c.endswith('_opp')]
-    norm_cols = [c for c in df.columns if not c.endswith('_opp')]
+def expand_team_opp_rows(df):
+    id_cols = ["team", "opp", "week", "Year", "homeAway"]
 
-    opp_cols += ['week', 'Year']
+    team_stats = [c for c in df.columns if not c.endswith("_opp") and c not in id_cols]
+    opp_stats = [c for c in df.columns if c.endswith("_opp")]
 
-    df_opp_w_suffix = df[opp_cols]
-    df_norm_no_suffix = df[norm_cols]
+    df_team = df.copy()
+    df_opp = df.copy()
 
-    df_opp_no_suffix = df_opp_w_suffix.copy()
-    df_opp_no_suffix.columns = df_opp_w_suffix.columns.str.removesuffix('_opp')
-    df_norm_w_suffix = df_norm_no_suffix.add_suffix('_opp', axis = 1)
+    df_opp["team"], df_opp["opp"] = df["opp"], df["team"]
 
-    df_main = pd.merge(df_norm_no_suffix, df_opp_no_suffix, on = ["team", 'week', 'Year'], how = 'inner')
-    return df_main
+    for c in team_stats:
+        df_opp[c] = df[c + "_opp"]
+
+    for c in opp_stats:
+        base = c.replace("_opp", "")
+        df_opp[c] = df[base]
+
+    df_out = pd.concat([df_team, df_opp], ignore_index=True)
+
+    return df_out
 
 if __name__ == "__main__":
     df = pd.read_csv("../CFB_predictions_take_2/post_calc_data/combined_data.csv")
-    df = dup_cols(df)
+    df = expand_team_opp_rows(df)
     print(df.columns)
+    df.to_csv("../CFB_predictions_take_2/check_data/check_dup_cols.csv", index=False, encoding="utf-8")
